a0a5a91954a43c86b12c2e275d9000d566165455
oananovac <novac.oana@yahoo.com>
2025-09-28 21:59:54 +0300

    Initial commit

---
+++ src/main/java/com/example/ss_2022_c21_e3/Ss2022C21E3Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_2022_c21_e3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C21E3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2022C21E3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss_2022_c21_e3/Ss2022C21E3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2022_c21_e3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C21E3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
